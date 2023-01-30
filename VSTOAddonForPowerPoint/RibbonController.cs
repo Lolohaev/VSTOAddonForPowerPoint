f18fd77b34033a54d407d940e42b42110c923e74
Ruslan Lolohaev <32525762+Lolohaev@users.noreply.github.com>
2023-01-30 05:25:50 +0300

    Добавьте файлы проекта.

---
+++ VSTOAddonForPowerPoint/RibbonController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VSTOAddonForPowerPoint.Properties;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace VSTOAddonForPowerPoint
+{
+    [ComVisible(true)]
+    public class RibbonController : Microsoft.Office.Core.IRibbonExtensibility
+    {
+        private Microsoft.Office.Core.IRibbonUI _ribbonUi;
+
+        public string GetCustomUI(string ribbonID) =>
+            @"<customUI xmlns='http://schemas.microsoft.com/office/2009/07/customui'>
+                        <ribbon>
+                           <tabs>
+                                <tab id='sample_tab' label='Custom button'>
+                                    <group id='sample_group' label='Operations'>
+                                        <button id='do_1' label='Do diagram' size='large' onAction='OnClick'/>
+                                    </group>
+                                </tab>
+                            </tabs>
+                        </ribbon>
+                    </customUI>";
+
+        public void OnLoad(Microsoft.Office.Core.IRibbonUI ribbonUI)
+        {
+            _ribbonUi = ribbonUI;
+        }
+
+        public async void OnClick(Microsoft.Office.Core.IRibbonControl control)
+        {
+            PowerPoint.Slide currentSlide = Globals.ThisAddIn.Application.ActivePresentation.Slides[1];
+            var chart = currentSlide.Shapes.AddChart(Office.XlChartType.xlColumnClustered, 0, 0, 500, 500).Chart;           
+            chart.ChartData.Activate();
+
+            if (chart.HasLegend)  chart.Legend.Font.Bold= true;
+
+            chart.ApplyDataLabels(XlDataLabelsType.xlDataLabelsShowValue);
+            
+        }
+        
+    }
+}
---
+++ VSTOAddonForPowerPoint/ThisAddIn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace VSTOAddonForPowerPoint
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            //this.Application.PresentationNewSlide += new PowerPoint.EApplication_PresentationNewSlideEventHandler( Application_PresentationNewSlide);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject() => new RibbonController();
+        void Application_PresentationNewSlide(PowerPoint.Slide Sld)
+        {
+            PowerPoint.Shape textBox = Sld.Shapes.AddTextbox(
+                Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
+            textBox.TextFrame.TextRange.InsertAfter("This text was added by using code.");
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
